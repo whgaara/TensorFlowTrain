9721b6df23c256902e40d7cf2c5d981c11eb963a
whgaara <695509060@qq.com>
2019-04-23 22:35:59 +0800

    'up'

--- LSTM-Classify/Classify.py
+++ LSTM-Classify/Classify.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 
+from keras.utils import to_categorical
 import tensorflow as tf
 import pymysql
 import csv
@@ -7,7 +8,7 @@ import pandas
 
 
 def init_data():
-    connect = pymysql.connect(host='', port=, user='', passwd='',
+    connect = pymysql.connect(host='', port=3306, user='', passwd='',
                               db='', use_unicode=True, charset="utf8")
     cursor = connect.cursor()
     cursor.execute('select result, predict from comment_sentence_new where predict in '
@@ -22,7 +23,29 @@ class LSTM_C():
     def __init__(self):
         self.source_data = pandas.read_csv('data.csv', encoding='utf-8')
 
-    def vectorize(self):
+    def labels2num(self, labels):
+        l2n = {}
+        for i, label in enumerate(set(labels)):
+            if label not in l2n:
+                l2n[label] = i
+        return l2n
+
+    def labels_vectorize(self):
+        self.labels = self.source_data['label']
+        l2n = self.labels2num(self.labels)
+        self.labels_onehot_list = to_categorical(list(l2n.values()))
+        self.labels_onehot_dict = {}
+        for key in self.labels:
+            self.labels_onehot_dict[key] = self.labels_onehot_list[l2n[key]]
+
+
+if __name__ == '__main__':
+    lc = LSTM_C()
+    lc.labels_vectorize()
+    re = lc.labels_onehot_list
+    re = lc.labels_onehot_dict
+    pass
+
 
 
 
