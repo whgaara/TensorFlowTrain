33738503084600f819baf3b8876e38bfec6dcdca
hang.wang <hang.wang@baozun.com>
2019-04-28 20:04:39 +0800

    'up'

--- LSTM-Classify/Classify.py
+++ LSTM-Classify/Classify.py
@@ -26,7 +26,7 @@ def init_data():
 class VOC_LSTM():
 
     batch_size = 100
-    epoch_count = 10
+    epoch_count = 20
     embedding_size = 200
     num_units = 100
     train_rate = 0.9
@@ -34,7 +34,7 @@ class VOC_LSTM():
     best_acc = 0.0
 
     def __init__(self):
-        self.source_data = pandas.read_csv('data_lite.csv', encoding='utf-8')
+        self.source_data = pandas.read_csv('data.csv', encoding='utf-8')
         self.source_data['labels_num'] = 0
         self.count_labels = len(set(self.source_data['labels']))
         self.source_data['labels_onehot'] = None
@@ -89,8 +89,7 @@ class VOC_LSTM():
             for j in range(batch_count):
                 yield self.train.iloc[j*VOC_LSTM.batch_size:min((j+1)*VOC_LSTM.batch_size, len(self.train))]
 
-    def lstm_model(self):
-        # tf.set_random_seed(VOC_LSTM.random_seed)
+    def train(self):
         x = tf.placeholder(tf.int32, [None, self.content_max_len], name='x')
         y = tf.placeholder(tf.int32, [None, self.num_classes], name='y')
 
@@ -100,7 +99,6 @@ class VOC_LSTM():
         # embedding
         vocab_size = len(self.vocab_processor.vocabulary_)
         embeddings = tf.Variable(tf.random_uniform([vocab_size, VOC_LSTM.embedding_size], -1.0, 1.0), name='embeddings')
-        # embeddings = tf.get_variable("embeddings", [vocab_size, VOC_LSTM.embedding_size], dtype=tf.float32)
         input_embedding = tf.nn.embedding_lookup(embeddings, x, name='input_embedding')
 
         # dropout
@@ -118,12 +116,8 @@ class VOC_LSTM():
         correct_prediction = tf.equal(prediction_num, tf.arg_max(y, 1), name='correct_prediction')
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name='accuracy')
 
-
-
         with tf.Session() as sess:
-            # with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
-            # saver = tf.train.Saver({'weights': weights, 'biases': biases, 'embeddings': embeddings})
             saver = tf.train.Saver()
             for epoch in range(VOC_LSTM.epoch_count):
                 for batch in self.__train_batch_iter():
@@ -135,18 +129,9 @@ class VOC_LSTM():
                 test_batch_labels = self.test['labels_onehot'].apply(pandas.Series).values
                 w, b, emb, acc = sess.run([weights, biases, embeddings, accuracy], feed_dict={x: test_batch_word2vec, y: test_batch_labels})
                 print(acc)
-                # p = sess.run(input_embedding, feed_dict={x: test_batch_word2vec, y: test_batch_labels})
-                # print(p)
-                # print('\n')
-                # labels_dict = self.source_data[['labels', 'labels_num']].drop_duplicates()
-                # p_n = sess.run(prediction_num, feed_dict={x: test_batch_word2vec, y: test_batch_labels})
-                # for i, content in enumerate(self.test['content']):
-                #     label = labels_dict[labels_dict['labels_num'] == p_n[i]]['labels']
-                #     print(content, label)
                 if acc > VOC_LSTM.best_acc:
                     saver.save(sess, 'model/my_net.ckpt')
                     VOC_LSTM.best_acc = acc
-            pass
 
     def record_info(self):
         # 记录本模型各个标签对应的num
@@ -172,46 +157,10 @@ class VOC_LSTM():
         sess = tf.Session()
         saver = tf.train.import_meta_graph('model/my_net.ckpt.meta')
         saver.restore(sess, "model/my_net.ckpt")
-
-        # sess.run(tf.global_variables_initializer())
-        # sess.run(tf.local_variables_initializer())
-
-        # from tensorflow.python import pywrap_tensorflow
-        # reader = pywrap_tensorflow.NewCheckpointReader('model/my_net.ckpt')
-        # var_to_shape_map = reader.get_variable_to_shape_map()
-        # for key in var_to_shape_map:
-        #     print("tensor_name: ", key)
-
-        # tf.reset_default_graph()
-        # weights = tf.get_variable("weights", shape=[VOC_LSTM.num_units, self.num_classes])
-        # embeddings = tf.get_variable("embeddings", shape=[363, 200])
-        graph = tf.get_default_graph()
-        embeddings = graph.get_tensor_by_name("embeddings:0")
-        embeddings = sess.run('embeddings:0', feed_dict={'x:0': word2nums})
         prediction_num = sess.run('prediction_num:0', feed_dict={'x:0': word2nums})
         for i, content in enumerate(discrete_content):
             label = labels_dict[labels_dict['labels_num'] == prediction_num[i]]['labels']
             print(content, label)
-
-        # saver = tf.train.Saver()
-        # with tf.Session() as sess:
-
-        # with tf.Session() as sess:
-        #     sess.run(tf.global_variables_initializer())
-        #     saver.restore(sess, 'model/my_net.ckpt')
-        #     graph = tf.get_default_graph()
-        #     x = graph.get_tensor_by_name("x:0")
-        #     p = sess.run('add:0', feed_dict={'x:0': word2nums})
-        #     print(p)
-        #     prediction_num = sess.run('prediction_num:0', feed_dict={'x:0': word2nums})
-        #     f = sess.run('rnn/while/Exit_3:0', feed_dict={'x:0': word2nums})
-        #     w = sess.run('weights:0')
-        #     b = sess.run('biases:0')
-        #     embeddings = sess.run('embeddings:0')
-        #     inputs = sess.run('input_embedding:0', feed_dict={'x:0': word2nums})
-        #     for i, content in enumerate(discrete_content):
-        #         label = labels_dict[labels_dict['labels_num'] == prediction_num[i]]['labels']
-        #         print(content, label)
 
 
 if __name__ == '__main__':
@@ -219,8 +168,5 @@ if __name__ == '__main__':
     # lc.vectorize()
     # lc.split_data()
     # lc.record_info()
-    # lc.lstm_model()
-    lc.use_model('码 鞋子 有点 小  ')
-    # source_data = pandas.read_csv('data_lite.csv', encoding='utf-8')
-    # lc.use_model(source_data['content'][1:100])
-    # lc.use_model(np.array(['天 到 ','买 有点 大 ','破 裤子 还 不能 处理','合适','码 正好 ','天 就 到 ','腰围 裤 长 穿 很 合适 ','鞋 非常 合适 ','多 性价比 非常 很 高 ','码 合适 裤子 有 弹性','赞 ','脚 码 正好 ','活动 也 给力 ']))
+    # lc.train()
+    lc.use_model(np.array(['天 到 ','买 有点 大 ','破 裤子 还 不能 处理','合适','码 正好 ','天 就 到 ','腰围 裤 长 穿 很 合适 ','鞋 非常 合适 ','多 性价比 非常 很 高 ','码 合适 裤子 有 弹性','赞 ','脚 码 正好 ','活动 也 给力 ']))
